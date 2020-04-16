741f9dfd9dba6ea46a6c7859513ee24a2462c547
Garrett Estrin <garrett.estrin@gmail.com>
2020-04-15 20:52:30 -0700

    Login and Add post calling api now

--- src/Utilities/PostUtilities.js
+++ src/Utilities/PostUtilities.js
@@ -2,10 +2,10 @@ import {sendGet, sendPost} from './DataUtilities.js';
 
 import env from './env.js';
 
-let {API_URL, HOME_URL} = env;
+let {API_URL} = env;
 let paginatedPostsEndpoint = "/post/get/paginated/";
 let postCountEndpoint = "/post/count";
-let createPostEndpoint = "/wp-admin/admin-ajax.php";
+let createPostEndpoint = "/post/add";
 
 export const getPaginatedPosts = (page = 0) => {
     if(page === "") {
@@ -21,6 +21,8 @@ export const getPostCount = () => {
 export const createPost = (data) => {
   let formData = new FormData();
   formData.append("caption", data.caption);
-  formData.append("image", data.file);
-  return sendPost(HOME_URL + createPostEndpoint + "?action=" + data.action, formData);
+  formData.append("file", data.file);
+  formData.append("name", data.name);
+  formData.append("type", data.type);
+  return sendPost(API_URL + createPostEndpoint, formData);
 }
--- src/Utilities/UserUtilites.js
+++ src/Utilities/UserUtilites.js
@@ -4,16 +4,23 @@ import env from './env.js';
 
 let {API_URL, HOME_URL} = env;
 let getUserRoleEndpoint = "/user/get/role";
-let sendUserLoginEndpoint = "/wp-admin/admin-ajax.php";
+let sendUserLoginEndpoint = "/user/login";
+let sendResetPasswordEndpoint = "/wp/wp-admin/admin-ajax.php";
 
 export const getUserRole = () => {
   return sendGet(API_URL + getUserRoleEndpoint);
 }
 
 export const sendLoginRequest = (data) => {
+  return sendPost(API_URL + sendUserLoginEndpoint + "?email=" + data.email + "&password=" + data.password);
+}
+
+export const sendResetPasswordRequest = (data) => {
+  return {data: {
+    success: true
+  }}
   let formData = new FormData();
   formData.append("email", data.email);
-  formData.append("password", data.password);
-  return sendPost(HOME_URL + sendUserLoginEndpoint + "?action=" + data.action, formData);
+  return sendPost(HOME_URL + sendResetPasswordEndpoint + "?action=" + data.action, formData);
 }
 
