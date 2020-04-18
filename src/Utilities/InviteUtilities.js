c7b2341f0a4e63309cc3c0f31d26bb8745c481ce
Garrett Estrin <garrett.estrin@gmail.com>
2020-04-17 18:37:50 -0700

    Fixed invite data request

--- src/Utilities/InviteUtilities.js
+++ src/Utilities/InviteUtilities.js
@@ -6,9 +6,5 @@ let {API_URL} = env;
 let sendInviteRequestEndpoint = "/user/request/invite";
 
 export const sendInviteRequest = (data) => {
-    let invite = {
-        name: data.name,
-        email: data.email
-    }
-    return sendPost(API_URL + sendInviteRequestEndpoint, JSON.stringify(invite));
+    return sendPost(API_URL + sendInviteRequestEndpoint + data);
 }
