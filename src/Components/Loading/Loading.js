0dc923e680fa1ae702d8952e33196300386995b1
Garrett Estrin <garrett.estrin@gmail.com>
2020-07-24 19:56:43 -0700

    Added loading progress bar for uploading images

--- src/Components/Loading/Loading.js
+++ src/Components/Loading/Loading.js
@@ -4,16 +4,35 @@ import './Loading.scss';
 import loadingImg from './assets/img/loading.jpg';
 
 class Loading extends Component {
+
+    calculateLoadedPercentage = (uploadProgress) => {
+        return Math.round((uploadProgress.loaded/uploadProgress.total)*100);
+    }
+
     render() {
         let { uploadProgress } = this.props;
-        
+        let uploadPercent;
+        if(uploadProgress) {
+            uploadPercent = this.calculateLoadedPercentage(uploadProgress);
+        }
         let loadingStyle = {
             backgroundImage: 'url(' + loadingImg + ')',
+        }
+        let loadingBarWidth = {
+            width: uploadPercent + "%"
         }
         return (
             <div className="loading container">
                 <div className="loading__icon" style={loadingStyle}></div>
-                {uploadProgress ? <p className="loading__text">{Math.round((uploadProgress.loaded/uploadProgress.total)*100)}% Uploaded</p> : <p className="loading__text">Loading...</p>}
+                {uploadProgress ? 
+                <div className="loading__text">
+                    <div className="loading__loading_bar-cont">
+                        <span className="loading__loaded_percent">{uploadPercent} % Uploaded</span>
+                        <div className="loading__loading_bar" style={loadingBarWidth}></div>
+                    </div>
+                </div> 
+                : 
+                <p className="loading__text">Loading...</p>}
                 
             </div>
         )
