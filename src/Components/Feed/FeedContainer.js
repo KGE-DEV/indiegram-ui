b1ef246534cc4fac0c7ce10824ba21d9e9e07fff
Garrett Estrin <garrett.estrin@gmail.com>
2020-09-19 11:35:44 -0700

    Removed last visited logic because it is stupid

--- src/Components/Feed/FeedContainer.js
+++ src/Components/Feed/FeedContainer.js
@@ -4,17 +4,6 @@ import {useParams} from 'react-router-dom';
 import Feed from './Feed.js';
 
 export default function FeedContainer(props) {
-    let lastVisitedFeed = handleLastFeedLastVisited();
     let {page} = useParams();
-    return <Feed page={lastVisitedFeed ? lastVisitedFeed : page} userRole={props.userRole}/>
-}
-
-function handleLastFeedLastVisited () {
-    if(localStorage && localStorage.lastVisitedFeed) {
-        let lastVisitedFeed = localStorage.lastVisitedFeed;
-        localStorage.removeItem("lastVisitedFeed");
-        return lastVisitedFeed;
-    } else {
-        return null;
-    }
+    return <Feed page={page} userRole={props.userRole}/>
 }
